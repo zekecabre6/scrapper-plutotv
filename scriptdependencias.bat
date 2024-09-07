2cf153b4e92ea526e732a2f058e705b43a29db4a
ezequiel cabrera <92662432+zekecabre6@users.noreply.github.com>
2024-09-07 01:09:34 -0300

    instalar dependencias por cmd

---
+++ scriptdependencias.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Instalando dependencias...
+
+pip install selenium webdriver_manager
+
+echo El paquete 'csv' ya está incluido en la biblioteca estándar de Python, no es necesario instalarlo.
+
+echo Dependencias instaladas correctamente.
+pause
